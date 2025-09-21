b13762cac5210a5dc45451a0adebef829d2fa9a1
bharatthakare <bharatthakare017@gmail.com>
2025-09-21 09:49:53 +0530

    Fix: ready for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
-const withPWA = require("next-pwa")({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-});
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  eslint: { ignoreDuringBuilds: true },
   images: { unoptimized: true },
+
+  // ⚠️ disable PWA temporarily
+  // ...withPWA({
+  //   dest: "public",
+  //   register: true,
+  //   skipWaiting: true,
+  // }),
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
